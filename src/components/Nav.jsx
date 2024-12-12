df182f3a41e5964f9862b5917208ea482ef5911f
pk11345 <pk11345rai@gmail.com>
2024-12-12 20:36:55 +0530

    Your descriptive commit message

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,13 +10,13 @@ const Nav = () => {
     </div>
     <ul className='social flex items-center justify-around w-[45%]'>
         <li>
-            <a href="https://web.whatsapp.com/"className='text-white' target='blank'>
+            <a href="https://web.whatsapp.com/"className='text-violet-800 font-bold' target='blank'>
             <img src="\icons8-whatsapp-100.png" alt=""className='icon h-[60px] ' />9708902003</a></li>
         <li>
-            <a href="https://www.instagram.com/bshivaonline?igsh=andwcDl1ajhpbDUy" className='text-white' target='blank'>
+            <a href="https://www.instagram.com/bshivaonline?igsh=andwcDl1ajhpbDUy" className='text-violet-800 font-bold' target='blank'>
             <img src="\icons8-instagram-500.png" alt="" className='icon h-[60px] ' />Instagram</a></li>
         <li>
-            <a href="https://t.me/+n44kyspP8KlmNDc1" target='blank' className='text-white'>
+            <a href="https://t.me/+n44kyspP8KlmNDc1" target='blank' className='text-violet-800 font-bold'>
             <img src="\icons8-telegram-100.png" alt="" className='icon h-[60px]' />Telegram</a></li>
     </ul>
     </div>
