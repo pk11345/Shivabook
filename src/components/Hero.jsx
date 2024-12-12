1330070027710298b4f24b0bc55b1eb3e1c6706e
pk11345 <pk11345rai@gmail.com>
2024-12-12 16:53:59 +0530

    image is showin

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   
     <div className="Heroimage w-full fixed h-[100vh]  ">
        
-        <img src="src\assets\hero image.jpeg" alt=""  className='w-[100%] h-[100%] absolute top-0 brightness-75'/>
+        <img src="\hero image.jpeg" alt=""  className='w-[100%] h-[100%] absolute top-0 brightness-75'/>
     </div>
     </>
   )
