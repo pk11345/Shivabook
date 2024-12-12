0bcfc9e08d8e9972eaab07df0f4ae6eb8b65f282
pk11345 <pk11345rai@gmail.com>
2024-12-12 20:32:37 +0530

    Your descriptive commit message

--- src/components/HeroContent.jsx
+++ src/components/HeroContent.jsx
@@ -6,7 +6,7 @@ const HeroContent = () => {
    <div className='heroContent w-full mt-[100px] flex justify-center items-center animate-pulse'>
    <div className="content w-[700px]  bg-black p-10 border-[10px] border-violet-400 rounded-3xl bg-opacity-60">
     <h1 className='text-white flex flex-col items-center'>
-        <span className='book text-[50px] font-extrabold'>Bookshiva</span>
+        <span className='book text-[50px] font-extrabold'>Shivabook</span>
         <span className='bet text-[45px] font-medium italic text-violet-400'>Ultimate Betting Platform</span></h1>
    </div>
    </div>
