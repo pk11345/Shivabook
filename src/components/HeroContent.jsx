3702f95c43893f46be67f8c452efa6a0a7781838
pk11345 <pk11345rai@gmail.com>
2024-12-19 22:14:31 +0530

    Your descriptive commit message

--- src/components/HeroContent.jsx
+++ src/components/HeroContent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HeroContent = () => {
   return (
     <>
-      <div className="heroContent flex justify-center items-center h-screen">
+      <div className="heroContent flex justify-center items-center mt-[100px] animate-pulse">
         <div className="content w-4/5 max-w-3xl bg-black p-10 border-4 border-violet-400 rounded-3xl bg-opacity-70">
           <h1 className="text-white text-center">
             <span className="block text-5xl font-extrabold">Shivabook</span>
